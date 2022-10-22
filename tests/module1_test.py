fac9bf149c62e73de2beae55c07c7227991ad0ca
Ioana Mihailescu <ioana.mihailescu20@imperial.ac.uk>
2022-10-22 10:35:25 +0100

    refactor tests
    Co-authored-by: Khemi <khemichew@users.noreply.github.com>
    Co-authored-by: Dilshaan Singh Grewal <DilshaanGrewal@users.noreply.github.com>

--- tests/module1_test.py
+++ tests/module1_test.py
@@ -14,7 +14,7 @@ class Module1Test(unittest.TestCase):
         model = Sequential(ff_layers)
         self.assertEqual(import_model(Framework.KERAS, model), model)
     
-    def test_import_model_not_keras(self):
+    def test_import_unsupported_model(self):
         model = "Not Keras Model!"
         self.assertRaises(ValueError, import_model, Framework.KERAS, model)
         
@@ -29,12 +29,18 @@ class Module1Test(unittest.TestCase):
 
     # Tests for approach 2: training a model given parameters
     def test_import_dataset(self):
-        filepath = "test_data.csv"
+        filepath = "data/test_data.csv"
         data_entries, labels = import_dataset(filepath)
-        # print(data_entries)
-        # print(labels)
         self.assertEqual(data_entries.shape, (3, 2))
         self.assertEqual(labels.shape, (3,))
+        
+    def test_import_dataset_features(self):
+        filepath = "data/test_data.csv"
+        data_entries, labels = import_dataset(filepath, ["name"])
+        self.assertEqual(data_entries.shape, (3, 1))
+        self.assertEqual(labels.shape, (3,))
+
+    def test_
         
 
 if __name__ == '__main__':
