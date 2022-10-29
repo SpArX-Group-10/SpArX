7c8c7295c985a44a0ad6eefcd0b690580573dc3f
dsg20 <dsg20@ic.ac.uk>
2022-10-29 13:28:11 +0100

    fix: abstract common initalisation

--- test/user_input_test.py
+++ test/user_input_test.py
@@ -4,13 +4,29 @@ from keras.layers import Activation, Dense, Input # pylint: disable=import-error
 from src.user_input import import_dataset, import_model, Framework, verify_keras_model_is_fnn # pylint: disable=import-error
 
 # Testing approach 1: importing a model
-def test_import_model_keras_and_fnn():
-    """ Import a Keras FFNN model. """
+def keras_fnn_model():
+    """ Returns a keras fnn model. """
     ff_layers = [
         Dense(10, activation='relu'),
         Dense(2, activation='softmax')
     ]
-    model = Sequential(ff_layers)
+    return Sequential(ff_layers)
+
+def keras_not_fnn_model():
+    """ Returns a keras model that is not a fnn. """
+    ff_layers = [
+        Input(shape=(10,)),
+        Dense(10, activation='relu'),
+        Dense(2, activation='softmax'),
+        Activation('relu')
+    ]
+    return Sequential(ff_layers)
+
+
+def test_import_model_keras_and_fnn():
+    """ Import a Keras FFNN model. """
+    model = keras_fnn_model()
+
     assert import_model(Framework.KERAS, model) == model
 
 
@@ -25,13 +41,7 @@ def test_import_unsupported_model():
 
 def test_import_model_keras_and_not_fnn():
     """ Fails when importing a Keras model that is not FFNN. """
-    ff_layers = [
-        Input(shape=(10,)),
-        Dense(10, activation='relu'),
-        Dense(2, activation='softmax'),
-        Activation('relu')
-    ]
-    model = Sequential(ff_layers)
+    model = keras_not_fnn_model()
 
     with pytest.raises(ValueError) as exc_info:
         import_model(Framework.KERAS, model)
@@ -47,47 +57,38 @@ def test_import_model_unsupported_framework():
 
 def test_verify_model_is_fnn():
     """ Returns true when Keras model that is FFNN. """
-    ff_layers = [
-        Dense(10, activation='relu'),
-        Dense(2, activation='softmax'),
-    ]
-    model = Sequential(ff_layers)
+    model = keras_fnn_model()
 
     assert verify_keras_model_is_fnn(model)
 
 
 def test_verify_model_is_not_fnn():
     """ Returns false when Keras model that is not FFNN. """
-    ff_layers = [
-        Input(shape=(10,)),
-        Dense(10, activation='relu'),
-        Dense(2, activation='softmax'),
-        Activation('relu')
-    ]
-    model = Sequential(ff_layers)
+    model = keras_not_fnn_model()
 
     assert not verify_keras_model_is_fnn(model)
 
 
 # Tests for approach 2: training a model given parameters
+USER_INPUT_DATA_FILEPATH = "test/data/test_user_input_data.csv"
+
 def test_import_dataset():
     """ Imports raw dataset and process the data. """
-    filepath = "test/data/test_user_input_data.csv"
-    data_entries, labels = import_dataset(filepath)
+    data_entries, labels = import_dataset(USER_INPUT_DATA_FILEPATH)
     assert data_entries.shape == (3, 2)
     assert labels.shape == (3,)
 
 
 def test_import_dataset_features():
     """ Imports raw dataset with selected features. """
-    filepath = "test/data/test_user_input_data.csv"
-    data_entries, labels = import_dataset(filepath, ["name"])
+    data_entries, labels = import_dataset(USER_INPUT_DATA_FILEPATH, ["name"])
     assert data_entries.shape == (3, 1)
     assert labels.shape == (3,)
 
 
 # def test_train_model():
 #     self.assertTrue(False and "not implemented")
+
 
 # def test_recall_m():
 #     self.assertTrue(False and "not implemented")
