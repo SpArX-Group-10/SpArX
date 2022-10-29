9b73613c30497fa0104d3dba104ab74ca13cc861
dsg20 <dsg20@ic.ac.uk>
2022-10-29 23:09:47 +0100

    fix: remove unused import

--- test/user_input_test.py
+++ test/user_input_test.py
@@ -2,7 +2,6 @@ import pytest
 from keras.models import Sequential # pylint: disable=import-error
 from keras.layers import Activation, Dense, Input # pylint: disable=import-error
 from src.user_input import import_dataset, import_model, Framework, verify_keras_model_is_fnn # pylint: disable=import-error
-from src.user_input import train_model # pylint: disable=import-error
 
 # Testing approach 1: importing a model
 def keras_fnn_model():
