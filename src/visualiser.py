799c69a61ed97f78c93b07fbc5bc98602858ab7e
Ioana Mihailescu <ioana.mihailescu20@imperial.ac.uk>
2022-10-30 19:41:16 +0000

    fix: Fix input labels and node positions.
    Co-authored-by: Mihnea <Mihneaghitu@users.noreply.github.com>

--- src/visualiser.py
+++ src/visualiser.py
@@ -1,10 +1,12 @@
 from abc import abstractmethod
+from typing import Optional
+
 import networkx as nx
 from bokeh.io import output_file, show
 from bokeh.plotting import from_networkx
 from bokeh.models import (BoxZoomTool, HoverTool, Plot, Range1d, ResetTool, PanTool, WheelZoomTool, MultiLine, Circle)
 from bokeh.palettes import Spectral4
-
+# from bokeh.models import Arrow, OpenHead
 
 from ffnn import FFNN
 
@@ -29,10 +31,7 @@ class Visualiser:
 class SimpleVisualizer(Visualiser):
     """ Generates a basic representation of the clustered model using networkx """
     @staticmethod
-    def _generate_networkx_model(mlp: FFNN) -> tuple[nx.DiGraph, dict]:
-        # TODO: add meaning of labels to the FFNN class
-        labels_text = ["petal_length", "petal_width", "sepal_length", "sepal_width"]
-
+    def _generate_networkx_model(mlp: FFNN, attr_names: list[str]=None) -> tuple[nx.DiGraph, dict]:
         G = nx.DiGraph()
 
         mlp_shapes = mlp.get_shape()
@@ -44,11 +43,22 @@ class SimpleVisualizer(Visualiser):
             mlp_idx_range.append((offset, offset+shape))
             offset = offset + shape
 
+        if attr_names is None:
+            attr_names = [f"X{idx}" for idx in range(mlp_shapes[0])]
+        else:
+            assert len(attr_names) == mlp_shapes[0]
+
+
+        # 3 -> 0.99 1.98 2.97
+        # 4 -> 0.75 1.50 2.25 3
         # calculate the position of each node
         pos_nodes = {}
+        SCALING_FACTOR = 3
         for layer, (start, end) in enumerate(mlp_idx_range):
-            #node_dist = FIG_HEIGHT / mlp_shapes[layer]
-            pos_nodes.update({n : (layer, i * 2) for i, n in enumerate(range(start, end))})
+            num_nodes = end - start + 1
+            # node_dist = FIG_HEIGHT / mlp_shapes[layer]
+            pos_nodes.update({n : (layer, (1 / num_nodes) * (i + 1) * SCALING_FACTOR) \
+                             for i, n in enumerate(range(start, end))})
 
         # loop through the weights of each layer and add them to the graph
         for l_idx, layer in enumerate(mlp.model.layers):
@@ -62,7 +72,7 @@ class SimpleVisualizer(Visualiser):
         new_labels = {}
         for layer, (start, end) in enumerate(mlp_idx_range):
             if layer == 0:
-                new_labels.update({lnode_idx: labels_text[lnode_idx] for lnode_idx in range(len(labels_text))})
+                new_labels.update({lnode_idx: attr_names[lnode_idx] for lnode_idx in range(len(attr_names))})
             elif layer == len(mlp_idx_range) - 1:
                 new_labels.update({start + out_idx: f"O{out_idx}" for out_idx in range(end - start + 1)})
             else:
@@ -75,32 +85,54 @@ class SimpleVisualizer(Visualiser):
         return (G, new_pos_nodes)
 
     @staticmethod
-    def visualise(mlp: FFNN) -> None:
+    def visualise(mlp: FFNN, features: Optional[list[str]] = None) -> None:
         """ Generate interactive visualisation using networkx and bokeh.
         :params
             mlp: FFNN
                 the network to be visualised
         """
-        (G, pos_nodes) = SimpleVisualizer._generate_networkx_model(mlp)
+        (G, pos_nodes) = SimpleVisualizer._generate_networkx_model(mlp, features)
 
-        plot = Plot(width=1920, height=1080, x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
+        WIDTH = 720
+        HEIGHT = 480
 
         ATTACK, SUPPORT = "red", "green"
         edge_colors = {}
         edge_weights = {}
+        edge_type = {}
         for start_node, end_node, d in G.edges(data=True):
             edge_color = ATTACK if d['weight'] < 0 else SUPPORT
             edge_colors[(start_node, end_node)] = edge_color
             edge_weights[(start_node, end_node)] = d['weight']
+            edge_type[(start_node, end_node)] = "Attack" if d['weight'] < 0 else "Support"
 
         nx.set_edge_attributes(G, edge_colors, "edge_color")
         nx.set_edge_attributes(G, edge_weights, "edge_weight")
+        nx.set_edge_attributes(G, edge_type, "edge_type")
 
-        graph = from_networkx(G, pos_nodes, scale=0.2, center=(360, 240))
+        graph = from_networkx(G, pos_nodes)
 
         node_hover_tool = HoverTool(tooltips=[("index", "@index")], renderers=[graph.node_renderer])
-        edge_hover_tool = HoverTool(tooltips=[("edge_weight", "@edge_weight")],
+        edge_hover_tool = HoverTool(tooltips=[("edge_weight", "@edge_weight"), ("edge_type", "@edge_type")],
                                     renderers=[graph.edge_renderer], line_policy='interp')
+
+
+        plot = Plot(width=WIDTH, height=HEIGHT, x_range=Range1d(-1.1, 1.1),
+                    y_range=Range1d(-1.1, 1.1), sizing_mode='scale_both')
+
+        # add arrows
+        # OFFSET_VAL = 0.025
+        # for start_node, end_node, d in G.edges(data=True):
+        #     arrow = Arrow(end=OpenHead(line_color='black', size=10),
+        #           y_start=pos_nodes[start_node][1],
+        #           x_start=pos_nodes[start_node][0],
+        #           x_end=pos_nodes[end_node][0] - OFFSET_VAL,
+        #           y_end=pos_nodes[end_node][1] + OFFSET_VAL if pos_nodes[start_node][1] > pos_nodes[end_node][1] \
+        #                                             else pos_nodes[end_node][1] - OFFSET_VAL,
+        #           line_width=0)
+        #     plot.add_layout(arrow)
+
+
         plot.add_tools(node_hover_tool, edge_hover_tool, BoxZoomTool(), ResetTool(), PanTool(), WheelZoomTool())
 
         graph.edge_renderer.glyph = MultiLine(line_color="edge_color", line_alpha=0.8, line_width="edge_weight")
@@ -108,4 +140,4 @@ class SimpleVisualizer(Visualiser):
         plot.renderers.append(graph)
 
         output_file("networkx_graph.html")
-        show(plot)
+        show(plot, sizing_mode='stretch_both')
