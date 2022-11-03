8a345ed3cee602b5fb0a8caec27bf720cb15d413
Unevilicorn <unevilicorn@gmail.com>
2022-11-03 19:50:51 +0000

    fix: pylint issues

--- src/gui/gui_data_manager.py
+++ src/gui/gui_data_manager.py
@@ -1,15 +1,15 @@
 
-class GUIDataManager:    
+class GUIDataManager:
     """Class to manage the data for the GUI."""
     def __init__(self):
         self.path = None
 
         self.has_labels = False
         self.has_ids = False
-        
+
         self.dataset = None
         self.lables = None
-        
+
 
 
         self.selected_inputs = None
