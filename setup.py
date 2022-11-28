97f53469248e3ee49f267f1f1cf6e1989f8f59d1
Unevilicorn <unevilicorn@gmail.com>
2022-11-28 22:53:53 +0000

    feat: restructure + pip

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("./README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("./requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.readlines()
+
 setuptools.setup(
-    name="Sparx",
+    name="sparx",
     version="1.0.0",
     author="Sparx Group",
     author_email="",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SpArX-Group-10/SpArX",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=(
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
