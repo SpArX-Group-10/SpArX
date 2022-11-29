7ae08d7c229cfc52fd378c75b4c3c347cb3e6e8a
Unevilicorn <unevilicorn@gmail.com>
2022-11-29 15:27:31 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import setuptools
+import os
 
-with open("./README.md", "r", encoding="utf-8") as fh:
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+readme_path = os.path.join(current_dir, "README.md")
+with open(readme_path, "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("./requirements.txt", "r", encoding="utf-8") as fh:
+requirements_path = os.path.join(current_dir, "requirements.txt")
+with open(requirements_path, "r", encoding="utf-8") as fh:
     requirements = fh.readlines()
 
 setuptools.setup(
